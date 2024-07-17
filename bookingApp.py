faad87d8c424c09132be616b71c5e2daffba19b7
BookingsStreamlit <166061925+BookingsStreamlit@users.noreply.github.com>
2024-07-17 11:54:21 +0100

    Update bookingApp.py

--- bookingApp.py
+++ bookingApp.py
@@ -37,7 +37,7 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", type="passwordApp", on_change=password_entered, key="passwordApp"
+        "Password", type="password", on_change=password_entered, key="passwordApp"
     )
     if "password_correct" in st.session_state:
         st.error("😕 Password incorrect")
