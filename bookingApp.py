ca27eac23d4570ecdb256ba0f27adae627889343
BookingsStreamlit <166061925+BookingsStreamlit@users.noreply.github.com>
2024-07-17 11:56:05 +0100

    Update bookingApp.py

--- bookingApp.py
+++ bookingApp.py
@@ -25,7 +25,7 @@ def check_password():
 
     def password_entered():
         """Checks whether a password entered by the user is correct."""
-        if hmac.compare_digest(st.session_state["password"], st.secrets["passwordApp"]):
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
             st.session_state["password_correct"] = True
             del st.session_state["password"]  # Don't store the password.
         else:
@@ -37,7 +37,7 @@ def check_password():
 
     # Show input for password.
     st.text_input(
-        "Password", type="password", on_change=password_entered, key="passwordApp"
+        "Password", type="password", on_change=password_entered, key="password"
     )
     if "password_correct" in st.session_state:
         st.error("😕 Password incorrect")
